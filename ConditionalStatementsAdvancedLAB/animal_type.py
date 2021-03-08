3eb37f4d90e1b885511e17239a5c5d434a3ec9ae
Valsorim98 <vt.stanchev@gmail.com>
2021-03-08 19:06:38 +0200

    edit animal_type task

---
+++ ConditionalStatementsAdvancedLAB/animal_type.py
@@ -0,0 +1,9 @@
+ANIMAL = str(input())
+
+if ANIMAL == "dog":
+    print("mammal")
+elif ANIMAL == "crocodile" or ANIMAL == "tortoise" or ANIMAL == "snake":
+    print("reptile")
+else:
+    print("unknown")
+    
