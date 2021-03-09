afa62b0fa328de224c0540fe533feb3a15310172
Valsorim98 <vt.stanchev@gmail.com>
2021-03-09 15:32:47 +0200

    Optimization and refactoring

--- ConditionalStatementsAdvancedLAB/working_hours.py
+++ ConditionalStatementsAdvancedLAB/working_hours.py
@@ -1,10 +1,24 @@
-HOUR = int(input())
-DAY = str(input())
+import sys
+
+HOUR = int(input("Please type an hour of the day "))
+DAY = str(input("Please type a day of the week "))
+
+work_days = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+weekend = ("Saturday", "Sunday")
+
+def is_working_day(DAY):
+
+    return DAY in work_days
+
+def is_weekend(DAY):
+
+    return DAY in weekend
 
 if 10 <= HOUR <= 18:
-    if DAY in ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"):
-        print("open")
-    else:
-        print("closed")
+    if is_working_day(DAY):
+        print("It's open")
+
+    elif is_weekend(DAY):
+        print("It's closed")
 else:
     print("closed")
