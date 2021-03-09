84266089e6fe4ff6d8202a6be081066ecadb1be1
Valsorim98 <vt.stanchev@gmail.com>
2021-03-09 16:43:21 +0200

    Add docstrings

--- ConditionalStatementsAdvancedLAB/fruit_or_vegetable.py
+++ ConditionalStatementsAdvancedLAB/fruit_or_vegetable.py
@@ -1,8 +1,33 @@
-PRODUCT = str(input())
+PRODUCT = str(input("Please type a fruit or vegetable: "))
 
-if PRODUCT in ("banana", "apple", "kiwi", "cherry", "lemon", "grapes"):
+fruits = ("banana", "apple", "kiwi", "cherry", "lemon", "grapes")
+vegetables = ("tomato", "cucumber", "pepper", "carrot")
+
+def is_fruit(PRODUCT):
+    """Shows if the product is a fruit.
+
+    Args:
+        PRODUCT (str): Product name.
+
+    Returns:
+        bool: Returns True if its a fruit, else False.
+    """    
+    return PRODUCT in fruits
+
+def is_vegetable(PRODUCT):
+    """Shows if the product is a vegetable.
+
+    Args:
+        PRODUCT (str): Product name.
+
+    Returns:
+        bool: Returns True if the product is a vegetable, else False.
+    """    
+    return PRODUCT in vegetables
+
+if is_fruit(PRODUCT):
     print("fruit")
-elif PRODUCT in ("tomato", "cucumber", "pepper", "carrot"):
+elif is_vegetable(PRODUCT):
     print("vegetable")
 else:
-    print("unknown")
+    print(f"no {PRODUCT} found!")
